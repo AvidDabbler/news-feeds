293dce2de0c74f8cccbfb1b82a656292408dfa4c
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-11-02 18:03:46 -0500

    update to toggle through sections

--- app.js
+++ app.js
@@ -1,42 +1,61 @@
 import {key} from './config.js'
 
-        const app = document.getElementById('app');
-        const nytkey = key();
-        
-        const getStories = ()=>{
-            fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=' + nytkey)
-            .then(response =>{
-                if(response.ok){
-                    return response.json();
-                }else{
-                    return Promise.reject(response);
-                }                  
-            }).then(data => {
-                console.log(data.results);
-                app.innerHTML = data.results.map(d=>{
-                    // if(d.multimedia[2].url !=undefined){
-                    //     const pic ="<img src" + d.multimedia[2].url + "alt=" + d.multimedia[2].url + ">";
-                    // }
-                    const link = "<a href='" + d.url+ "'><h2>" + d.title + "</h2></a>"
-                    if(d.multimedia.length > 1){
-                        const pic = d.multimedia[2].url;
-                        return(
-                            "<div class='container' id='" + d.url + "'><div class='about'><div class='pic'><img src='" 
-                            + pic +
-                            "' ></div><div class='headline'>" + link + "<h3>" 
-                            + d.byline + "</h3><p>" + d.abstract + "</p></div></div></div>"
-                        );
-                    }
-                    return(
-                        "<div class='container' id='" + d.url + "'><h2>"
-                        + link + "</h2><h3>" + d.byline + "</h3><p>" + d.abstract + "</p></div></div>"
+const home = document.getElementById('home');
+const tech = document.getElementById('tech');
+const world = document.getElementById('world');
+const politics = document.getElementById('politics');
 
-                        );
-                    }).join('');
-            }).catch(err => {
-                console.log("Error logged: ", err);
-            });
+const nytkey = key();
+const render=(articles, section)=>{
+    section[1].innerHTML = "<h1>" + section[2] + "</h1>" + articles.map(d=>{
+        const link = "<a href='" + d.url+ "'><h2>" + d.title + "</h2></a>";
+        if(d.multimedia.length > 1){
+            const pic = d.multimedia[2].url;
+            return(
+                "<div class='container' id='" + d.url + "'><div class='about'><div class='pic'><img src='" 
+                + pic +
+                "' ></div><div class='headline'>" + link + "<h3>" 
+                + d.byline + "</h3><p>" + d.abstract + "</p></div></div></div>"
+            );
         }
+        return(
+            "<div class='container' id='" + d.url + "'><h2>"
+            + link + "</h2><h3>" + d.byline + "</h3><p>" + d.abstract + "</p></div></div>"
 
-        getStories();
-	
+            );
+        }).join('');
+}
+const list = [['home', home, "Top News"],['technology', tech, "Technology News"],['world', world, "World News"],['politics', politics, "Political News"]];
+    list.forEach(s=>{
+                fetch('https://api.nytimes.com/svc/topstories/v2/' + s[0] + '.json?api-key=' + nytkey)
+                .then(response =>{
+                    if(response.ok){
+                        return response.json();
+                    }else{
+                        return Promise.reject(response);
+                    }                  
+                }).then(data => {
+                    render(data.results, s);
+                }).catch(err => {
+                    console.log("Error logged: ", err);
+                });
+            });
+
+const sectDisp = (tab)=>{
+    home.style.display='none';
+    tech.style.display='none';
+    world.style.display='none';
+    politics.style.display='none';
+
+    tab.style.display = 'block';
+}
+
+
+
+homeBtn.addEventListener('click', event => sectDisp(home), false)
+techBtn.addEventListener('click', event => sectDisp(tech), false)
+worldBtn.addEventListener('click', event => sectDisp(world), false)
+politicsBtn.addEventListener('click', event => sectDisp(politics), false)
+
+
+sectDisp(home);
